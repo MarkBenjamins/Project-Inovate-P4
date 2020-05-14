e54b4d3bcf19f90f03598db0c410fb860160f5ff
tesramaker <56303174+tesramaker@users.noreply.github.com>
2020-05-14 16:23:51 +0200

    basis

---
+++ Innovate/newsfeed.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <title></title>
+    </head>
+    <body>
+        <div class="header">
+            <div class="white">
+                <h2>10:00</h2>
+            </div>
+            <div class="white">
+                <h2>zon</h2>
+            </div>
+            <div class="white">
+                <h2>vrijdag 14 mei 2020, weeknr. 21</h2>
+            </div>
+            <div class="darkmode">
+                <button><img src="" alt=""></button>
+            </div>
+        </div>
+        <div class="worksheet">
+            <h1>NEWSFEED</h1>
+        </div>
+        <div class="aanwezigheid">
+            <p>Docenten</p>
+        </div>
+    </body>
+</html>
