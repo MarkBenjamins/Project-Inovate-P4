1ed4c30e6c6dc34d3703a7c888f202b8b289918a
tesramaker <56303174+tesramaker@users.noreply.github.com>
2020-05-18 14:24:46 +0200

    Bootstrap toegevoegd

--- Innovate/newsfeed.php
+++ Innovate/newsfeed.php
@@ -3,28 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="bootstrap-grid.css" type="text/css">
         <title></title>
     </head>
     <body>
-        <div class="header">
-            <div class="white">
+        <div class="row header">
+            <div class="col-2 white">
                 <h2>10:00</h2>
             </div>
-            <div class="white">
+            <div class="col-1 white">
                 <h2>zon</h2>
             </div>
-            <div class="white">
+            <div class="col-6 white">
                 <h2>vrijdag 14 mei 2020, weeknr. 21</h2>
             </div>
-            <div class="darkmode">
-                <button><img src="" alt=""></button>
+            <div class="col-1">
+                <button class="darkmode"><img src="" alt=""></button>
             </div>
         </div>
-        <div class="worksheet">
-            <h1>NEWSFEED</h1>
-        </div>
-        <div class="aanwezigheid">
-            <p>Docenten</p>
+        <div class="row">
+            <div class="col-10 worksheet">
+                <h1>NEWSFEED</h1>
+            </div>
+            <div class="col-1 aanwezigheid">
+                <p>Docenten</p>
+            </div>
         </div>
     </body>
 </html>
