f5c66e518568b5dfc9c24311ea8c95ea5419bfde
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-05-18 12:39:48 +0200

    Create login.php
    Backend Inlogsysteem.
    Basis frame voor inloggen en een basis validate voor input.
    
    Er ontbreekt nog veel dus moet nog worden upgedate

---
+++ login.php
@@ -0,0 +1,38 @@
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <label for="Username">Username:</label>
+    <input type="text" name="Username" placeholder="Username"><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" placeholder="Password"><br>
+
+    <input type="submit" name="submit" value="Login"><br>
+</form>
+
+<?php
+if (isset($_POST["submit"])) 
+{
+    // Als invoer-velden leeg zijn error.
+    if (empty($_POST["password"]) || empty($_POST["Username"])) 
+    {
+        echo "Please fill in all fields ", $_SERVER["PHP_SELF"];
+    }
+    else
+    {
+        $Username = $_POST["Username"];
+        $password = $_POST["password"];
+
+        // hier moet db naam nog bij in
+        $connect = mysqli_connect("localhost", "root", "", "");
+
+        // hier moet een goede query
+        $sql = "SELECT * FROM gebruikers WHERE `gebruikersnaam` = ?";
+
+        if ($stmt = mysqli_prepare($connect, $sql)) 
+        {
+
+           // stuk code om de data te versturen en te controleren
+
+        }
+    }
+}
+?>
