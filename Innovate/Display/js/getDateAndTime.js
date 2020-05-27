00e96f38fcae68ace46240d5b34d31100ecea302
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-05-27 17:12:47 +0200

    Create getDateAndTime.js
    1 functie gemaakt van
    getTime & getDate
    i.v.m het niet werken van afzondelijke functies
    
    Co-Authored-By: jennyve <jennyve@users.noreply.github.com>

---
+++ Innovate/Display/js/getDateAndTime.js
@@ -0,0 +1,40 @@
+
+/**
+ * Functie om de datum van het device op te halen.
+ * Note: Dit gebreurd bij elke refresh.
+ * @return {date} De huidige datum van het device.
+ */
+function getCurrentDate()
+{
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth();
+    let day = today.getDate();
+    let date = day +  "-" + month + "-" + year;
+    //let date = year + "/" + month + "/" + day;
+    let element1 = document.getElementById('date');
+    element1.innerHTML = date;
+    return date;
+}
+
+/**
+ * Functie om de tijd van het device op te halen.
+ * Note: Dit gebreurd bij elke refresh.
+ * @return {time} De huidige tijd van het device.
+ */
+function getTime()
+{
+    let date = getCurrentDate()
+    let today = new Date();
+    let hr = today.getHours();
+    let min = today.getMinutes();
+    //let sec = today.getSeconds();
+    //let time = hr + ":" + min + ":" + sec;
+    let time = hr + ":" + min;
+    let element = document.getElementById('time');
+    element.innerHTML = time;
+    return time;
+}
+
+window.onload = getTime;
+
