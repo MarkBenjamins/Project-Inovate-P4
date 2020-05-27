381f4f944b444299cff8b2e964eb1e38410e5ddb
Niels Exel <42837867+hiiamnielse@users.noreply.github.com>
2020-05-27 15:21:33 +0200

    data.json naar weatherdata.json
    naam van json file aangepast

--- Innovate/Display/weerbericht.php
+++ Innovate/Display/weerbericht.php
@@ -1,16 +1,16 @@
 <?php
-$cache_file = 'data.json';
+$cache_file = 'weatherdata.json';
 if(file_exists($cache_file)){
-  $data = json_decode(file_get_contents($cache_file));
+  $weatherdata = json_decode(file_get_contents($cache_file));
 }else{
   $api_url = 'https://content.api.nytimes.com/svc/weather/v2/current-and-seven-day-forecast.json';
-  $data = file_get_contents($api_url);
-  file_put_contents($cache_file, $data);
-  $data = json_decode($data);
+  $weatherdata = file_get_contents($api_url);
+  file_put_contents($cache_file, $weatherdata);
+  $weatherdata = json_decode($weatherdata);
 }
 
-$current = $data->results->current[0];
-$forecast = $data->results->seven_day_forecast;
+$current = $weatherdata->results->current[0];
+$forecast = $weatherdata->results->seven_day_forecast;
 
 ?>
 
