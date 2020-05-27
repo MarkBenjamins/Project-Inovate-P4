c2c86f48bfbcd14c5305f291e93dfccec816e7ff
Niels Exel <42837867+hiiamnielse@users.noreply.github.com>
2020-05-27 15:37:42 +0200

    weer iconen

--- Innovate/Display/weerbericht.php
+++ Innovate/Display/weerbericht.php
@@ -3,15 +3,15 @@ $cache_file = 'weatherdata.json';
 if(file_exists($cache_file)){
   $weatherdata = json_decode(file_get_contents($cache_file));
 }else{
-  $api_url = 'https://content.api.nytimes.com/svc/weather/v2/current-and-seven-day-forecast.json';
+  $api_url = 'http://weerlive.nl/api/json-data-10min.php?key=00ac1da8ca&locatie=Emmen';
   $weatherdata = file_get_contents($api_url);
   file_put_contents($cache_file, $weatherdata);
   $weatherdata = json_decode($weatherdata);
 }
 
-$current = $weatherdata->results->current[0];
-$forecast = $weatherdata->results->seven_day_forecast;
-
+$current = $weatherdata->liveweer[0];
+$temperatuur= $current->temp;
+$samenvatting =$current->samenv;
 ?>
 
 
@@ -21,7 +21,7 @@ $forecast = $weatherdata->results->seven_day_forecast;
       return $value;
     }else if($unit=='F'){
       $cen = ($value - 32) / 1.8;
-      	return round($cen,0);
+      	return round($cen,2);
       }
   }
 ?>
