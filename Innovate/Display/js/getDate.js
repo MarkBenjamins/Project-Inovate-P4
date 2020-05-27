fb1e30007215947d1df4d5cf86e59f9ad9c88aba
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-05-27 15:27:07 +0200

    Comments toegevoegd
    commentaar op de functies toegevoegd

--- Innovate/Display/js/getDate.js
+++ Innovate/Display/js/getDate.js
@@ -1,3 +1,8 @@
+/**
+ * Functie om de datum van het device op te halen.
+ * Note: Dit gebreurd bij elke refresh.
+ * @return {date} De huidige datum van het device.
+ */
 function getCurrentDate(){
     let today = new Date();
     let year = today.getFullYear();
--- Innovate/Display/js/getTime.js
+++ Innovate/Display/js/getTime.js
@@ -1,8 +1,13 @@
+/**
+ * Functie om de tijd van het device op te halen.
+ * Note: Dit gebreurd bij elke refresh.
+ * @return {time} De huidige tijd van het device.
+ */
 function getTime(){
     let today = new Date();
     let hr = today.getHours();
     let min = today.getMinutes();
     //let sec = today.getSeconds();
-    let time = hr + ":" + min ;
+    let time = hr + ":" + min;
     return time;
 }
