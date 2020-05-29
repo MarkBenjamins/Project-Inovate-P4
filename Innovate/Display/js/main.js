52d9426a709bdaaf490c812cfa286e45fdb7dbd2
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-05-29 22:08:27 +0200

    Manual geschreven
    Handleiding toegevoegd.
    Bugs aangegeven in de code.
    volgorde veranderd van het aan roepen van de functies op bassis van de import volgorde

--- Innovate/Display/js/main.js
+++ Innovate/Display/js/main.js
@@ -1,13 +1,38 @@
-//hier worden alle functies aangeroepen
-
-//zet hier je import neer in het volgende formaat:
-//import { functieNaam } from 'fileLocatie';
+/**
+ * main functies
+ * hier worden alle functies aangeroepen
+ * 
+ * @manual
+ * Stap 1. 
+ * plaats je import in het volgende formaat:
+ * import { functieNaam } from 'fileLocatie';
+ * 
+ * Stap 2.
+ * roep daarna de functie aan
+ * 
+ * Stap 3.
+ * aan het eind van de file met de functie zet je een export in dit formaat:
+ * export { functienaam };
+ */
 import { getTime } from './getTime.js';
 import { getCurrentDate } from './getDate.js';
-import { getDetails } from './getDetails.js';
-//roep je functie aan
-getDetails();
+
+/**
+ * @note deze werkt niet
+ * @Storm_Visser kun jij dit oplossen?
+ * import { getDetails } from './getDetails.js';
+*/
+
 getTime();
+
 getCurrentDate();
-//aan het eind van de file met de functie zet je een export in dit formaat:
-//export { functienaam }; 
+
+/**
+ * @note deze werkt niet
+ * @Storm_Visser kun jij dit oplossen?
+ * //getDetails();
+*/
+
+
+
+
