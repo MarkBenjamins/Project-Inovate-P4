283ab13415c203e5dd0aee8218a4c43ac4b3d8b8
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-05-31 19:37:47 +0200

    Test_login verbeterd.
    Inlog validatie vebeterd.
    
    Als je inlogd en het is niet goed krijg je een tekst melding en geen alarm.
    
    Wachtwoord word gecheckt  op veel punten.

--- Innovate/Display/js/getUsernamAndPassword.js
+++ Innovate/Display/js/getUsernamAndPassword.js
@@ -1,4 +1,6 @@
-// Loop through Array of Objects
+/**
+ * Array list die de JSON file moet voorstellen.
+ */
 var user = 
 [
 	{ // user @ 0 index
@@ -16,29 +18,114 @@ var user =
 ]
 
 /**
+ * Constanten voor de {checkUsernameAndPassword()} functie.
+ */
+const username = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+/**
+ * Functie om de user input voor username en password te valideren.
+ * Na de validate met de array user wordt de gebruiker doorgestuurd en stopt de code. 
+ */
+function checkUsernameAndPassword()
+{
+	form.addEventListener
+	('submit', (e) => 
+		{
+			// array voor de error meldingen.
+			let messages = []
+
+			// if input username is leeg error.
+			if (username.value === '' || username == null || username.value === ' ') 
+			{
+				messages.push('Username is required')
+			}
+
+			// if input password is leeg error.
+			else if (password.value === '' || password.value == null) 
+			{
+				messages.push('Password is required')
+			}
+
+			// if password bestaat uit minder dan 3 karakters error.
+			else if (password.value.length <= 3) 
+			{
+				messages.push('Password must be longer than 3 characters')
+			}
+
+			// if password beaat uit meer dan 19 karakters error.
+			else if (password.value.length >= 20) 
+			{
+				messages.push('Password must be less than 20 characters')
+			}
+
+			// if password is wachtwoord of password error.
+			else if (password.value === 'password'|| password.value === 'wachtwoord' 
+					|| password.value === 'Password'|| password.value === 'Wachtwoord') 
+			{
+				messages.push('Password cannot be password or wachtwoord')
+			}
+
+			// if input username en input password gelijk zijn aan username en password uit array user.
+			// ga naar de volgende pagina, anders geef een errror. 
+			else
+			{
+				var inputUsername = username.value
+				var inputPassword = password.value
+				// loop door de array list en vergelijk met user input.
+				for(var i = 0; i < user.length; i++) 
+				{
+					// Checkt of user input overeenkomt met een waarde die in de user array staat.
+					if(inputUsername == user[i].username && inputPassword == user[i].password) 
+					{
+						// stuur de gebruiker na validatie door naar de volgende pagina.
+						e.preventDefault()
+						alert(inputUsername + " is logged in!!!")
+						window.location.href = "Test_newsfeedLogin.html";
+						return
+					}
+				}
+				e.preventDefault()
+				messages.push("Incorrect username or password.")
+			}
+
+			// Als de input niet voldoet stuur dan de error naar het scherm
+			if(messages.length > 0) 
+			{
+				e.preventDefault()
+				errorElement.innerText = messages.join(', ')
+			}
+		}
+	)
+}
+
+/**
+ * @deprecated {getInfo()} validate is ingebouwd bij de checkUsernameAndPassword functie.
+ * Waardoor de check al gedaan wordt in de functie en is aangepast aan de eisen van het systeem.
  * Functie om de user input te valideren met de array.
  * Validate {true} Ingelogd melding, waarna je wordt doorgelinkt naar de ingelogde pagina.
  * Validate {false} Error melding.
  * @param {username} De input die de user invuld.
  * @param {password} De input die de user invuld.
  */
-function getInfo() 
-{
-    // user input naar var 
-	var username = document.getElementById('username').value
-	var password = document.getElementById('password').value
+// function getInfo() 
+// {
+// 	// user input naar var 
+// 	var username = document.getElementById('username').value
+// 	var password = document.getElementById('password').value
 
-    for(var i = 0; i < user.length; i++) 
-    {
-        // Checkt of user input overeenkomt met een waarde die in de objPeople array staat.
-        if(username == user[i].username && password == user[i].password) 
-        {
-			alert(username + " is logged in!!!")
-            // stop de functie als je ingelogd bent
-            window.location.href = "Test_newsfeedLogin.html";
-			return
-		}
-	}
-	alert("Incorrect username or password.")
-}
-
+//     for(var i = 0; i < user.length; i++) 
+//     {
+//         // Checkt of user input overeenkomt met een waarde die in de objPeople array staat.
+//         if(username == user[i].username && password == user[i].password) 
+//         {
+// 			alert(inputUsername + " is logged in!!!")
+//             // stop de functie als je ingelogd bent
+//             window.location.href = "Test_newsfeedLogin.html";
+// 			return
+// 		}
+// 	}
+// 	alert("Incorrect username or password.")
+//}
