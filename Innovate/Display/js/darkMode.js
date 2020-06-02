66a781e249e9b01e4c0f307b590c95f84eefb493
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-06-02 08:44:57 +0200

    TEST darkmode functie
    Geupdate i.v.m. code efficentie en comments toegevoegd.

--- Innovate/Display/js/darkMode.js
+++ Innovate/Display/js/darkMode.js
@@ -10,34 +10,10 @@ function swapStyleSheet(sheet)
 }
 
 /**
- * Functie van de Dark mode knop.
- * Zorgt er voor dat er gewisseld kan worden tussen:
- * - Darkstyle.css
- * - Lightstyle.css
- * Waardoor de styling van de pagina verandert wordt.
- * @param {*} button De knop waar op gedrukt wordt om te wisselen van css file.
+ * Functie om de inlog afbeelding te wijzigen
+ * @note Deze apparte functie is nodig voor de niet ingelogde pagina buienrader en newsfeed.
+ * @param {*} button De knop waar op gedrukt wordt om te wisselen van afbeelding.
  */
-function toggle(button) 
-{
-        switch (button.value) {
-            case "ONN":
-                swapStyleSheet('../Css/Darkstyle.css')
-                document.getElementById("docentLogoColorChange").src = "../img/icons-scherm/docent_wit.png";
-                document.getElementById("aanwezigheidLogoColorChange").src = "../img/icons-scherm/aanwezigheid_wit.png";
-                document.getElementById("addMessageLogoColorChange").src = "../img/icons-scherm/addmessage_wit.png";
-                button.value = "OFF";
-
-                break;
-            case "OFF":
-                swapStyleSheet('../Css/lightstyle.css')
-                document.getElementById("docentLogoColorChange").src = "../img/icons-scherm/docent.png";
-                document.getElementById("aanwezigheidLogoColorChange").src = "../img/icons-scherm/aanwezigheid.png";
-                document.getElementById("addMessageLogoColorChange").src = "../img/icons-scherm/addmessage.png";
-                button.value = "ONN";
-                break;
-    }
-}
-
 function toggleBasic(button) 
 {
         switch (button.value) {
@@ -48,13 +24,42 @@ function toggleBasic(button)
 
                 break;
             case "OFF":
-                swapStyleSheet('../Css/lightstyle.css')
+                swapStyleSheet('../Css/style.css')
                 document.getElementById("docentLogoColorChange").src = "../img/icons-scherm/docent.png";
                 button.value = "ONN";
                 break;
     }
 }
 
+/**
+ * Functie van de Dark mode knop.
+ * Zorgt er voor dat er gewisseld kan worden tussen:
+ * - Darkstyle.css
+ * - style.css
+ * Zorgt ook dat de afbeeldingen gewisseld worden tussen donker en wit.
+ * Waardoor de styling van de pagina verandert wordt.
+ * @param {*} button De knop waar op gedrukt wordt om te wisselen van style.
+ */
+function toggle(button) 
+{
+        switch (button.value) {
+            case "ONN":
+                swapStyleSheet('../Css/Darkstyle.css')
+                toggleBasic(button)
+                document.getElementById("aanwezigheidLogoColorChange").src = "../img/icons-scherm/aanwezigheid_wit.png";
+                document.getElementById("addMessageLogoColorChange").src = "../img/icons-scherm/addmessage_wit.png";
+                button.value = "OFF";
+
+                break;
+            case "OFF":
+                swapStyleSheet('../Css/style.css')
+                toggleBasic(button)
+                document.getElementById("aanwezigheidLogoColorChange").src = "../img/icons-scherm/aanwezigheid.png";
+                document.getElementById("addMessageLogoColorChange").src = "../img/icons-scherm/addmessage.png";
+                button.value = "ONN";
+                break;
+    }
+}
 
 // function iconsDark()
 // {
