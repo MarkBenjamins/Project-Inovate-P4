ed469557c5aa12b202721f5223181e1006a12a90
Avijn <38077967+Avijn@users.noreply.github.com>
2020-06-08 10:34:36 +0200

    Code cleanup

--- Innovate/include/DBFunction.php
+++ Innovate/include/DBFunction.php
@@ -1,52 +1,49 @@
 <?php
 /*
-   vv  Space to call Functions  vv  
+     Space to call Functions    
 */
 GetDocent();
-getdata();
-getJson();
+GetData();
+GetJson();
 
 /*
-**************************************************************************************************************
-**************************************************************************************************************
+******************************
+******************************
 */
-function getJson()
+
+function GetJson()
 {
     //header("Content-Type: application/json");
     $json = file_get_contents('teacher.json');
     $data =  json_decode($json);
 }
 
-function getdata()
+function GetData()
 {
-require "DBConnect.php";
+    require "DBConnect.php";
+    
     if($_POST){
-    echo("<br><br>");
-    var_dump($_POST);
+    
         $data = $_POST;
         $teacher= array_keys($data)[0];
         $status = $data[$teacher];
-        
-        echo("<br><br>");
-        print ($teacher ." heeft als status: ". $status);
-
-    $sql = "UPDATE `docent` SET Status=? WHERE id = ?";
-    if(!$stmt = mysqli_prepare($conn, $sql))
+        $sql = "UPDATE `docent` SET Status=? WHERE id = ?";
+    
+        if(!$stmt = mysqli_prepare($conn, $sql))
+        {
+           die("Could not prepare the given statment");
+        }
+        else 
+        {
+	        mysqli_stmt_bind_param($stmt, 'is', $status , $teacher);
+            mysqli_stmt_execute($stmt);
+        }
+    }
+    else
     {
-       die("Could not prepare the given statment");
-    }
-    else {
-	mysqli_stmt_bind_param($stmt, 'is', $status , $teacher);
-    mysqli_stmt_execute($stmt);
-}
-
-
-    //$obj = json_decode($_POST, true);
-    //print_r($obj);
-    }
-    else{
-    print("<br>Komt geen get/post binnen <br>");
+            echo("<br>Komt geen get/post binnen <br>");
 	}
+    
 }
 
 function GetDocent()
@@ -70,17 +67,8 @@ function GetDocent()
 		}	
         
         $myJSON = json_encode($obj, JSON_FORCE_OBJECT);
-        echo $myJSON;
         file_put_contents('teacher.json',$myJSON);
     }      
     mysqli_stmt_close($stmt);
 }
 
-function UpdateStatus()
-{
-    require "DBConnect.php";
-    $sql = "update `docent` SET `status` = ? WHERE `ID` = ?";
-
-    //HIER VERDER
-
-}
