5b9b37bb1654b7e12db04fdd51afa2f3dd1d8c5c
Storm-Visser <storm.visser@student.nhlstenden.com>
2020-06-18 11:42:19 +0200

    fix voor mark

--- Innovate/Display/js/getDetails.js
+++ Innovate/Display/js/getDetails.js
@@ -80,121 +80,121 @@ const docenten = [
     foto:" "
     }
 ]
-var xmlhttp = new XMLHttpRequest();
+    
 
-xmlhttp.open("GET", "DBFunction.php", true);
-xmlhttp.send();
-
-xmlhttp.onreadystatechange = function() 
+console.log(docenten)
+/**
+ * Functie om door de waardes van de JSON file heen te loopen.
+ * @note Het aantal loops is afhankelijk van de grote van de JSON File
+ */
+let count = 0;
+function getDetails()
 {
-    if (this.readyState == 4 && this.status == 200) 
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "teacher.json", true);
+
+    xmlhttp.onload = function () {
+        myObj = JSON.parse(xmlhttp.responseText);
+
+        docenten.unshift({voornaam: myObj[0].voornaam,achternaam: myObj[0].achternaam,status:myObj[0].status,foto: " "});
+    }
+
+    xmlhttp.send(null);
+
+    for (let index = 0; index < docenten.length; index++) 
     {
-        myObj = JSON.parse(this.responseText);
-        docenten.unshift({voornaam: myObj[1].voornaam},{achternaam: myObj[2].achternaam},{status:myObj[3].status},{foto: " "});
+        makeDetails()
     }
 }
-console.log(docenten)
-// /**
-//  * Functie om door de waardes van de JSON file heen te loopen.
-//  * @note Het aantal loops is afhankelijk van de grote van de JSON File
-//  */
-// let count = 0;
-// function getDetails()
-// {
-//     for (let index = 0; index < docenten.length; index++) 
-//     {
-//         makeDetails()
-//     }
-// }
 
-// /**
-//  * Functie om de status van de JSON file uit te lezen en aan de hand daarvan de kleur te bepalen.
-//  * @return {colour} De kleur die gelijk staat aan de status.
-//  */
-// function getColour()
-// {
-//     let colour;
-//     switch(docenten[count].status)
-//     {
-//         case 0:
-//             colour = "green"    //groen, aanwezig en beschikbaar
-//             break;
-//         case 1:
-//             colour = "red"      //rood, afwezig
-//             break;
-//         case 2:
-//             colour = "#ffe066"  //geel, aanwezig maar niet beschikbaar
-//             break;
-//         default:
-//             colour = "#737373"  //grijs, default als het onbekend is
-//     }
-//     return colour;
-// }
+/**
+ * Functie om de status van de JSON file uit te lezen en aan de hand daarvan de kleur te bepalen.
+ * @return {colour} De kleur die gelijk staat aan de status.
+ */
+function getColour()
+{
+    let colour;
+    switch(docenten[count].status)
+    {
+        case 0:
+            colour = "green"    //groen, aanwezig en beschikbaar
+            break;
+        case 1:
+            colour = "red"      //rood, afwezig
+            break;
+        case 2:
+            colour = "#ffe066"  //geel, aanwezig maar niet beschikbaar
+            break;
+        default:
+            colour = "#737373"  //grijs, default als het onbekend is
+    }
+    return colour;
+}
 
-// /**
-//  * Functie om de gegevens van de docent op te halen.
-//  * @return {details} Bevat de status kleur, fotolocatie, voornaam en achternaam van de gebruiker.
-//  */
-// function makeDetails()
-// {
-//     let colour = getColour();
-//     // voeg de voor en achternaam samen
+/**
+ * Functie om de gegevens van de docent op te halen.
+ * @return {details} Bevat de status kleur, fotolocatie, voornaam en achternaam van de gebruiker.
+ */
+function makeDetails()
+{
+    let colour = getColour();
+    // voeg de voor en achternaam samen
 
-//     let details = docenten[count].voornaam + " "+ docenten[count].achternaam;
+    let details = docenten[count].voornaam + " "+ docenten[count].achternaam;
 
 
-//     //maak een div aan waar de list elementen in kunnen
-//     let divElem = document.createElement("div");
-//     //maak een nieuw list element aan
-//     let listElem = document.createElement("li");
-//     //maak een nieuw div element aan
-//     let statusElem = document.createElement("div");
-//     //maak een span aan om de div in te zetten
-//     let statusSpanElem = document.createElement("span");
+    //maak een div aan waar de list elementen in kunnen
+    let divElem = document.createElement("div");
+    //maak een nieuw list element aan
+    let listElem = document.createElement("li");
+    //maak een nieuw div element aan
+    let statusElem = document.createElement("div");
+    //maak een span aan om de div in te zetten
+    let statusSpanElem = document.createElement("span");
 
-//     //maak er tekst voor dat nieuwe list element
-//     let textElem = document.createTextNode(details);
+    //maak er tekst voor dat nieuwe list element
+    let textElem = document.createTextNode(details);
 
-//     statusSpanElem.appendChild(statusElem)
-//     //koppel het statuselement aan het list element
-//     listElem.appendChild(statusSpanElem);
-//     //koppel het textelement aan het listelement
-//     listElem.appendChild(textElem);
-//     //koppel het list element aan het div element
-//     divElem.appendChild(listElem);
+    statusSpanElem.appendChild(statusElem)
+    //koppel het statuselement aan het list element
+    listElem.appendChild(statusSpanElem);
+    //koppel het textelement aan het listelement
+    listElem.appendChild(textElem);
+    //koppel het list element aan het div element
+    divElem.appendChild(listElem);
 
-//     //zet de stijl en de class van het statuselement aanhankelijk van de status
-//     statusElem.style.backgroundColor = colour;
-//     statusElem.setAttribute("class", "TeacherStatus");
-//     // statusElem.style.width = "5px";
-//     // statusElem.style.height = "30px";
-//     // statusElem.style.float = "left"
-//     // statusElem.style.marginRight = "5px"
-//     // statusElem.style.marginTop = "-5px"
-//     //set de class van het divElement
-//     if(count%2 == 0)
-//     {
-//         divElem.setAttribute("class", "TeacherBlock1");
-//     }
-//     else
-//     {
-//         divElem.setAttribute("class", "TeacherBlock2");
-//     }
+    //zet de stijl en de class van het statuselement aanhankelijk van de status
+    statusElem.style.backgroundColor = colour;
+    statusElem.setAttribute("class", "TeacherStatus");
+    // statusElem.style.width = "5px";
+    // statusElem.style.height = "30px";
+    // statusElem.style.float = "left"
+    // statusElem.style.marginRight = "5px"
+    // statusElem.style.marginTop = "-5px"
+    //set de class van het divElement
+    if(count%2 == 0)
+    {
+        divElem.setAttribute("class", "TeacherBlock1");
+    }
+    else
+    {
+        divElem.setAttribute("class", "TeacherBlock2");
+    }
 
-//     // divElem.style.height = "40px";
-//     // divElem.style.width = "110%";
-//     // divElem.style.marginBottom = "10px";
-//     // divElem.style.paddingTop = "10px";
-//     // divElem.style.paddingLeft = "10px";
-//     // divElem.style.backgroundColor = "white";
-//     // divElem.style.color = "black";
+    // divElem.style.height = "40px";
+    // divElem.style.width = "110%";
+    // divElem.style.marginBottom = "10px";
+    // divElem.style.paddingTop = "10px";
+    // divElem.style.paddingLeft = "10px";
+    // divElem.style.backgroundColor = "white";
+    // divElem.style.color = "black";
 
-//     //plak het gemaakte listelement onder het element van de UL
-//     document.getElementById("list").appendChild(divElem);
+    //plak het gemaakte listelement onder het element van de UL
+    document.getElementById("list").appendChild(divElem);
 
-//     //een counter voor de array
-//     count++;
-//     return details;
-// }
-// //window.onload(getDetails());
-// export { getDetails };
+    //een counter voor de array
+    count++;
+    return details;
+}
+//window.onload(getDetails());
+export { getDetails };
