acc22fd94d822e62ba76b74421e0b6eab56568b2
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2020-06-26 12:00:06 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/MarkBenjamins/Project-Inovate-P4""
    This reverts commit 5edc23b1aff0b28415aab7af0c85bb7190e649ee.

--- Innovate/Display/fileupload.php
+++ Innovate/Display/fileupload.php
@@ -1,34 +1,31 @@
 <?php
-//Get the base64 data from the array. Get index 1 that contains the needed base64 encoded data. Index 0 contains the type.
-$data = (explode(',', $_POST['value']))[1];
-
-$mysqli = new mysqli("localhost", "root", "", "innovate");
-
-/* check connection */
-if (mysqli_connect_errno()) 
+if(isset($_FILES['file']['name']))
 {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+   // file name
+   $filename = $_FILES['file']['name'];
+
+   // Location
+   $location = '../img/message/'.$filename;
+
+   // file extension
+   $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+   $file_extension = strtolower($file_extension);
+
+   // Valid image extensions
+   $valid_ext = array("jpg","png","jpeg","gif");
+
+   $response = 0;
+   if(in_array($file_extension,$valid_ext))
+   {
+      // Upload file
+      if(move_uploaded_file($_FILES['file']['tmp_name'],$location))
+      {
+         $response = 1;
+      } 
+   }
+   echo $response;
+
+   recuire ""
 }
 
-// It is in OOP but made sure i made it as readable as possible for you guys.
-/* create a prepared statement */
-if ($stmt = $mysqli->prepare("INSERT INTO `bericht`(`UserID`, `Link`, `ShowBericht`, `Foto`) VALUES (?, ?, ?, ?)")) 
-{
-    /* bind parameters for markers */
-    $stmt->bind_param("isis", $userId, $link, $showBericht, $foto);
-    $userId = 1;
-    $link = "I am a link";
-    $showBericht = 1;
-    $foto = $data;
-
-    /* execute query */
-    $stmt->execute();
-
-    /* close statement */
-    $stmt->close();
-}
-
-/* close connection */
-$mysqli->close();
 ?>
